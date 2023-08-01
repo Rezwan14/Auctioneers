d1abc1b0ade62415ea69884d46194b609eeb2e0d
Rhoenzfe <rhoenz.fermin@gmail.com>
2023-07-31 20:41:46 -0400

    make form as a pop up

--- src/components/AuctionForm.js
+++ src/components/AuctionForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
+import Modal from 'react-modal';
 
 const AuctionForm = ({ handleCreateAuction }) => {
-  const [showForm, setShowForm] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const [newItem, setNewItem] = useState({
     itemName: '',
     category: '',
@@ -11,10 +12,10 @@ const AuctionForm = ({ handleCreateAuction }) => {
     startDate: '',
     endDate: '',
   });
-  
+
   const createItem = (event) => {
     event.preventDefault();
-    handleCreateAuction(newItem.itemName,newItem.category,newItem.description,newItem.startingBid,newItem.startTime,newItem.startDate,newItem.endDate,
+    handleCreateAuction(newItem.itemName, newItem.category, newItem.description, newItem.startingBid, newItem.startTime, newItem.startDate, newItem.endDate,
     );
     setNewItem({
       itemName: '',
@@ -25,73 +26,98 @@ const AuctionForm = ({ handleCreateAuction }) => {
       startDate: '',
       endDate: '',
     });
-    setShowForm(false);
+    setShowModal(false);
   };
-  
+
   const handleInput = (event) => {
     const { name, value } = event.target;
     setNewItem({ ...newItem, [name]: value });
   };
-  
+
   return (
     <div>
-      <button onClick={() => setShowForm(true)}>Create Auction</button>
-      {showForm && (
+      <button onClick={() => setShowModal(true)}>Create Auction</button>
+      <Modal
+        isOpen={showModal}
+        onRequestClose={() => setShowModal(false)}
+        contentLabel='Create Auction Modal'
+      >
         <form onSubmit={createItem}>
+          <h1>Create Auction Form</h1>
           <div>
-          <input
-            type="text"
-            name="itemName"
-            placeholder="Item Name"
-            value={newItem.itemName}
-            onChange={handleInput}
-          />
+            Item name:
+            <input
+              type="text"
+              name="itemName"
+              placeholder="Item Name"
+              value={newItem.itemName}
+              onChange={handleInput}
+            />
           </div>
-          <input
-            type="text"
-            name="category"
-            placeholder="Category"
-            value={newItem.category}
-            onChange={handleInput}
-          />
-          <input
-            type="text"
-            name="description"
-            placeholder="Description"
-            value={newItem.description}
-            onChange={handleInput}
-          />
-          <input
-            type="number"
-            name="startingBid"
-            placeholder="Starting Bid"
-            value={newItem.startingBid}
-            onChange={handleInput}
-          />
-          <input
-            type="text"
-            name="startTime"
-            placeholder="Start Time"
-            value={newItem.startTime}
-            onChange={handleInput}
-          />
-          <input
-            type="date"
-            name="startDate"
-            placeholder="Start Date"
-            value={newItem.startDate}
-            onChange={handleInput}
-          />
-          <input
-            type="date"
-            name="endDate"
-            placeholder="End Date"
-            value={newItem.endDate}
-            onChange={handleInput}
-          />
+          <div>
+            Category:
+            <input
+              type="text"
+              name="category"
+              placeholder="Category"
+              value={newItem.category}
+              onChange={handleInput}
+            />
+          </div>
+          <div>
+            Description:
+            <input
+              type="text"
+              name="description"
+              placeholder="Description"
+              value={newItem.description}
+              onChange={handleInput}
+            />
+          </div>
+          <div>
+            Starting Bid:
+            <input
+              type="number"
+              name="startingBid"
+              placeholder="Starting Bid"
+              value={newItem.startingBid}
+              onChange={handleInput}
+            />
+          </div>
+          <div>
+            Start Time:
+            <input
+              type="text"
+              name="startTime"
+              placeholder="Start Time"
+              value={newItem.startTime}
+              onChange={handleInput}
+            />
+          </div>
+          <div>
+            Start Date:
+            <input
+              type="date"
+              name="startDate"
+              placeholder="Start Date"
+              value={newItem.startDate}
+              onChange={handleInput}
+            />
+          </div>
+          <div>
+            End Date:
+            <input
+              type="date"
+              name="endDate"
+              placeholder="End Date"
+              value={newItem.endDate}
+              onChange={handleInput}
+            />
+          </div>
           <button type="submit">Create</button>
         </form>
-      )}
+      </Modal>
+
     </div>
   );
 };
